6e12da75abadd5cd1615161b5a718d6263e1d6fe
Dhawal Bisht <dhawalbisht4543@gmail.com>
2023-07-25 13:20:36 +0530

    css file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import Header from './components/Header'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses'
