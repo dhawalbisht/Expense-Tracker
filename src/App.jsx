9f47db7b4842435ae669a9d401e274d4d5c70427
Dhawal Bisht <dhawalbisht4543@gmail.com>
2023-07-25 13:04:59 +0530

    balance component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import Balance from './components/Balance'
 
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
   return (
     <div>
       <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </div >
   )
 }
